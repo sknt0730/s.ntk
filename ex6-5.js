6ec46688175577b3656933e90a996c85107cd067
sknt0730 <zm19568@josai.ac.jp>
2021-06-08 05:04:02 +0000

    06081

---
+++ ex6-5.js
@@ -0,0 +1,12 @@
+var who65="world";
+function sayhello65(){
+    alert('Hello,'+who65 + '!');
+}
+function taro65(){
+    taro65='太郎'
+    alert(taro65+'が来た');
+}
+function hanako65(){
+    hanako65='花子'
+    alert(hanako65+'が来た')
+}
