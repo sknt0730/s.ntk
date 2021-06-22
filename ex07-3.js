53116d8f421e1d3f7ea4c1857f0a22ff103ed206
sknt0730 <zm19568@josai.ac.jp>
2021-06-22 14:52:49 +0000

    06222

--- ex07-3.js
+++ ex07-3.js
@@ -1,5 +1,16 @@
 function add(){
     var x = document.getElementById('input1').Value;
-    var y =document.getElementById('input2').Value;
-    alert(x+ y);
+    var y = document.getElementById('input2').Value;
+    x = Number(x);
+    y = Number(y);
+    alert(x+y);
+}
+
+function Bmi(){
+    var weight = document.getElementById('input3').value;
+    var height = document.getElementById('input4').value;
+    weight = Number(weight);
+    height = Number(height);
+    var bmi = height / (weight * weight);
+    alert("BMI値は"　+ bmi + "です。");
 }
