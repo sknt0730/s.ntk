b272cddb47c9f9c9743dfe9d29f270ced5862d3b
sknt0730 <zm19568@josai.ac.jp>
2021-06-26 14:08:27 +0000

    06261

--- ex07-3.js
+++ ex07-3.js
@@ -1,16 +1,5 @@
 function add(){
-    var x = document.getElementById('input1').Value;
-    var y = document.getElementById('input2').Value;
-    x = Number(x);
-    y = Number(y);
-    alert(x+y);
-}
-
-function Bmi(){
-    var weight = document.getElementById('input3').value;
-    var height = document.getElementById('input4').value;
-    weight = Number(weight);
-    height = Number(height);
-    var bmi = height / (weight * weight);
-    alert("BMI値は"　+ bmi + "です。");
+    var x = Number(document.getElementById('input1').Value);
+    var y = Number(document.getElementById('input2').Value);
+    alert(x +y );
 }
---
+++ ex07-31.js
@@ -0,0 +1,5 @@
+function Bmi(){
+    var w =Number( document.getElementById("weight").value);
+    var h =Number( document.getElementById("height").value);
+    alert( w /  (h / 100) *(h / 100)));
+}
